bf71c948df5213e77be1aebcc404efa4c9fee80c
Mohamed Adel <imoedev@gmail.com>
2023-04-13 09:49:46 +0200

    fixing the fcking hire me for the last time

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -77,7 +77,7 @@ const Navbar = () => {
             isOpen ? 'opacity-0' : 'opacity-100'
           }`}
         ></span>
-        <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm translate-y-0.5 ${
+        <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
             isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'
           }`}></span>
       </button>
