41f52f85a7ed4560b7a332d599c49f5e5dffa386
Mohamed Adel <imoedev@gmail.com>
2023-04-13 09:29:06 +0200

    updating hire me to fixed on mobile

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -5,7 +5,7 @@ import { CircularText } from './Icons';
 const HireMe = () => {
   return (
     <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden
-     md:right-8 md:left-auto md:top-8 md:bottom-auto md:absolute">
+     md:right-8 md:left-auto md:top-8 md:bottom-auto ">
       <div className="w-48 h-auto flex items-center justify-center relative md:w-32">
         <CircularText
           className={'fill-dark animate-spin-slow dark:fill-light'}
