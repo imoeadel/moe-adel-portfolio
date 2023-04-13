575263bbdf196cba515e257997d790676976e606
Mohamed Adel <imoedev@gmail.com>
2023-04-13 09:38:02 +0200

    fixing the fcking hire me

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -5,18 +5,18 @@ import { CircularText } from './Icons';
 const HireMe = () => {
   return (
     <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden
-      ">
-      <div className="w-48 h-auto flex items-center justify-center relative ">
+     md:right-8 md:left-auto md:top-8 md:bottom-auto md:absolute">
+      <div className="w-48 h-auto flex items-center justify-center relative md:w-32">
         <CircularText
           className={'fill-dark animate-spin-slow dark:fill-light'}
         />
         <Link
           href="mailto:imoedev@gmail.com"
-          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2
+          className="flex items-center justify-center text-center absolute left-1/2 top-1/2 -translate-x-1/2
            -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark 
            w-16 h-16 rounded-full text-sm font-semibold hover:bg-light hover:text-dark
            dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light
-           hover:dark:border-light "
+           hover:dark:border-light md:w-12 md:h-12 md:text-[10px]"
         >
           Hire Me
         </Link>
